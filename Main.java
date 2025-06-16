ddaaf27e6be9c0dce9c56e710b29b0de133c26fa
kubal <jirka.kubala@gmail.com>
2025-06-16 20:33:58 +0200

    prvni commit tj. odevzdani na github3

--- Main.java
+++ Main.java
@@ -4,5 +4,10 @@ public class Main {
 
         System.out.println("Neco na github...");
 
+        System.out.println("Neco na github22...");
+
+
+        System.out.println("Neco na github44...");
+
     }
 }
