7515f05216a40b661c8129e5d913f199adceb83e
jirkakubala <58389376+jirkakubala@users.noreply.github.com>
2025-06-16 21:54:48 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,6 @@ public class Main {
 
         System.out.println("Neco na github...");
 
-        System.out.println("Neco na github22...");
-
-
-        System.out.println("Neco na github44...");
-
         int vysledek = Matika.secist(10,2);
         System.out.println("soucet je " + vysledek);
 
